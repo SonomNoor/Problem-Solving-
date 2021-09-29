fe720a2b8a47314e4a7ed117fbda3e9ff9f8c95d
SonomRubayat <56910004+SonomRubayat@users.noreply.github.com>
2021-09-29 20:11:15 +0600

    Update reverse vowel.cpp

--- GFG problem Solution/reverse vowel.cpp
+++ GFG problem Solution/reverse vowel.cpp
@@ -2,6 +2,56 @@
 #include<iostream>
 using namespace std;
 
+bool isVowel (char c)
+{
+	return (c=='a' || c=='e'||c=='i'|| c=='o'|| c=='u'||
+c=='A' || c=='E'||c=='I'|| c=='O'|| c=='U');
+}
+	string modify (string s)
+	{
+		int n=s.size();
+		int front=0;
+		int back=n-1;
+		
+		while (front<back)
+		{
+			if (!isVowel(s[front]))
+			{
+			front++;
+			continue;
+		}
+			if (!isVowel(s[back])){
+			
+			back--;
+			continue;
+		}
+			
+			swap(s[front],s[back]);
+			front++;
+			back--;
+	
+		}
+		return s;
+	}
+
+
+int main ( )
+{
+	string s ;
+	cin>>s;
+cout <<	modify(s);
+	return 0;
+	
+	
+	
+	
+}
+
+
+---------////same solution in gfg format 
+#include<iostream>
+using namespace std;
+
 public:
         bool isVowel(char c) { 
         return (c=='a' || c=='A' || c=='e' || 
