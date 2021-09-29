2d425ba443857cfdfdc6af5e16e5c2e20454fe4a
SonomRubayat <56910004+SonomRubayat@users.noreply.github.com>
2021-09-29 20:32:25 +0600

    Update delete alternate character.cpp

--- GFG problem Solution/delete alternate character.cpp
+++ GFG problem Solution/delete alternate character.cpp
@@ -33,4 +33,37 @@ int main() {
     }
     return 0;
 }
-  /
+  //////////////////////////////another solution
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    string delAlternate(string S) {
+       int i,n=S.size();
+       string str("");
+       for(i=0; i<n; i++){
+          if(i%2==0)
+           str+=S[i];
+           
+       }
+       return str;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string S;
+        cin >> S;
+        Solution ob;
+        cout << ob.delAlternate(S) << endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
