46011455bb4b65569c4c02516f6bb0b05809f552
SonomRubayat <56910004+SonomRubayat@users.noreply.github.com>
2021-09-29 19:28:28 +0600

    Create reverse vowel.cpp

---
+++ GFG problem Solution/reverse vowel.cpp
@@ -0,0 +1,50 @@
+//reverse the vowels 
+#include<iostream>
+using namespace std;
+
+public:
+        bool isVowel(char c) { 
+        return (c=='a' || c=='A' || c=='e' || 
+                c=='E' || c=='i' || c=='I' || 
+                c=='o' || c=='O' || c=='u' || 
+                c=='U'); 
+        } 
+
+        string modify (string s)
+        {
+            int n = s.size();
+            int front = 0;
+            int back = n - 1;
+
+            while(front < back){
+                if(!isVowel(s[front])){
+                    front++;
+                    continue;
+                }
+
+                if(!isVowel(s[back])){
+                    back--;
+                    continue;
+                }
+
+                swap(s[front], s[back]);
+                front++;
+                back--;
+            }
+            return s;
+        }
+};
+
+int main ()
+{
+  t main()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        string s; cin >> s;
+        Solution ob;
+        cout <<ob.modify (s) << endl;
+    }
+}
+}
