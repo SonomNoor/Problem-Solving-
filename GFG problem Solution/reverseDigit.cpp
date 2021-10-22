bef507c4feab0466c1f3882a966a479505200be1
SonomRubayat <56910004+SonomRubayat@users.noreply.github.com>
2021-10-22 10:17:06 +0600

    Create reverseDigit.cpp

---
+++ GFG problem Solution/reverseDigit.cpp
@@ -0,0 +1,40 @@
+//Given N,  reverse the digits of N.
+
+
+// { Driver Code Starts
+
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution
+{
+	public:
+		long long int reverse_digit(long long int n)
+		{
+		    // Code here
+		    long res=0,rem=0;
+		    while (n>0)
+		    {
+		        rem=n%10;
+		        res=res*10+rem;
+		        n=n/10;
+		    }
+		
+return res;
+}
+};
+
+
+// { Driver Code Starts.
+int main(){
+    
+    
+    	long long int n;
+    	cin >> n;
+    	Solution ob;
+    	long long int  ans = ob.reverse_digit(n);
+    	cout << ans <<"\n";
+    
+	return 0;
+}  // } Driver Code Ends
